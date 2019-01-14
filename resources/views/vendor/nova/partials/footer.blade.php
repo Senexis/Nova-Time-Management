357d69a5bf312765c78118c70950993bd02bebf5
Senexis <timoviveen@live.nl>
2019-01-14 19:26:23 +0100

    Streamline app layout.

--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <p class="mt-8 text-center text-xs text-80">
-    <a href="https://github.com/Senexis/time-management-simulator" target="_blank" class="text-primary dim no-underline">Github</a>
+    <a href="https://github.com/senexis/time-management-simulator-redux" target="_blank" class="text-primary dim no-underline">Github</a>
     <span class="px-1">&middot;</span>
-    &copy; {{ date('Y') }} TMS Redux
+    &copy; {{ date('Y') }} {{ Nova::name() }}
     <span class="px-1">&middot;</span>
-    Made with Laravel Nova v{{ Laravel\Nova\Nova::version() }}
+    {{ __('nova.layout.footer_credit', ['version' => Laravel\Nova\Nova::version()]) }}
 </p>
