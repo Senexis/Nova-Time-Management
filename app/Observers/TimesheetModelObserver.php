1acc5e8d09905d48c0bea29057de964d7a45c3fd
Senexis <timoviveen@live.nl>
2019-01-15 23:12:11 +0100

    Handle tracked timesheet events in observer.

--- app/Observers/TimesheetModelObserver.php
+++ app/Observers/TimesheetModelObserver.php
@@ -31,8 +31,19 @@ class TimesheetModelObserver
      */
     public function updating($model)
     {
-        if ($model->ended_at != null && $model->type == 'manual') {
+        if ($model->type == 'manual' && $model->ended_at != null) {
             $model->time_worked = $model->ended_at->diffInSeconds($model->started_at);
+        }
+
+        if ($model->type == 'tracked') {
+            if ($model->ended_at == null && $model->paused_at != null) {
+                // Paused without prior resuming.
+                $model->time_worked += $model->paused_at->diffInSeconds($model->resumed_at ?? $model->started_at);
+            } else if ($model->ended_at != null && $model->paused_at == null) {
+                // Ended without prior pausing.
+                $model->time_worked += $model->ended_at->diffInSeconds($model->resumed_at ?? $model->started_at);
+                $model->resumed_at = null;
+            }
         }
     }
 }
