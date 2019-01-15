cae8ee6c163150929ea4fe40ca63f4b696aa38da
Senexis <timoviveen@live.nl>
2019-01-15 23:12:56 +0100

    TrackedTimesheets: Initial release.

--- nova-components/TrackedTimesheets/resources/js/components/Tool.vue
+++ nova-components/TrackedTimesheets/resources/js/components/Tool.vue
@@ -1,26 +1,199 @@
 <template>
-    <div>
-        <heading class="mb-6">Tracked Timesheets</heading>
-
-        <card class="bg-90 flex flex-col items-center justify-center" style="min-height: 300px">
-            <svg class="spin fill-80 mb-6" width="69" height="72" viewBox="0 0 23 24" xmlns="http://www.w3.org/2000/svg"><path d="M20.12 20.455A12.184 12.184 0 0 1 11.5 24a12.18 12.18 0 0 1-9.333-4.319c4.772 3.933 11.88 3.687 16.36-.738a7.571 7.571 0 0 0 0-10.8c-3.018-2.982-7.912-2.982-10.931 0a3.245 3.245 0 0 0 0 4.628 3.342 3.342 0 0 0 4.685 0 1.114 1.114 0 0 1 1.561 0 1.082 1.082 0 0 1 0 1.543 5.57 5.57 0 0 1-7.808 0 5.408 5.408 0 0 1 0-7.714c3.881-3.834 10.174-3.834 14.055 0a9.734 9.734 0 0 1 .03 13.855zM4.472 5.057a7.571 7.571 0 0 0 0 10.8c3.018 2.982 7.912 2.982 10.931 0a3.245 3.245 0 0 0 0-4.628 3.342 3.342 0 0 0-4.685 0 1.114 1.114 0 0 1-1.561 0 1.082 1.082 0 0 1 0-1.543 5.57 5.57 0 0 1 7.808 0 5.408 5.408 0 0 1 0 7.714c-3.881 3.834-10.174 3.834-14.055 0a9.734 9.734 0 0 1-.015-13.87C5.096 1.35 8.138 0 11.5 0c3.75 0 7.105 1.68 9.333 4.319C16.06.386 8.953.632 4.473 5.057z" fill-rule="evenodd"/></svg>
-
-            <h1 class="text-white text-4xl text-90 font-light mb-6">
-                We're in a black hole.
-            </h1>
-
-            <p class="text-white-50% text-lg">
-                You can edit this tool's component at:
-                <code class="ml-1 border border-80 text-sm font-mono text-white bg-black rounded px-2 py-1">/nova-components/TrackedTimesheets/resources/js/components/Tool.vue</code>
-            </p>
-        </card>
-    </div>
+  <loading-view :loading="initialLoading">
+    <h1 class="mb-6 text-90 font-normal text-2xl">Log Details</h1>
+    <loading-card
+      :loading="loading"
+      class="card overflow-hidden"
+    >
+      <div v-if="isValid">
+        <div class="px-8 py-6">{{ hours }}:{{ minutes }}:{{ seconds }}</div>
+        <div class="bg-30 flex px-8 py-4">
+          <button
+            class="btn btn-default btn-primary inline-flex items-center relative ml-auto mr-auto"
+            type="button"
+            v-if="isRunning"
+            @click="pauseTimer"
+          >Pause</button>
+          <button
+            class="btn btn-default btn-primary inline-flex items-center relative ml-auto mr-3"
+            type="button"
+            v-if="isPaused"
+            @click="resumeTimer"
+          >Resume</button>
+          <button
+            class="btn btn-default btn-primary inline-flex items-center relative mr-auto"
+            type="button"
+            v-if="isPaused"
+            @click="stopTimer"
+          >Stop</button>
+        </div>
+      </div>
+    </loading-card>
+  </loading-view>
 </template>
 
 <script>
 export default {
-    mounted() {
-        //
+  data: () => ({
+    initialLoading: true,
+    loading: false,
+    runningTimer: {},
+    timer: null,
+    timeWorked: 0
+  }),
+
+  methods: {
+    initialize() {
+      this.getRunningTimer();
     },
-}
+
+    getRunningTimer() {
+      Nova.request()
+        .get("/nova-vendor/tracked-timesheets/latest-timer")
+        .then(response => {
+          this.runningTimer = response.data;
+
+          if (this.isValid) {
+            this.timeWorked = response.data.time_worked;
+
+            if (this.isRunning) {
+              this.startTimer();
+            }
+          } else {
+            // TODO: Move to create new.
+          }
+
+          this.initialLoading = false;
+          this.loading = false;
+        });
+    },
+
+    pauseRunningTimer() {
+      this.loading = true;
+
+      Nova.request()
+        .get("/nova-vendor/tracked-timesheets/latest-timer/pause")
+        .then(response => {
+          this.runningTimer = response.data;
+
+          if (this.isValid) {
+            this.timeWorked = response.data.time_worked;
+          } else {
+            // TODO: Move to create new.
+          }
+
+          this.loading = false;
+        });
+    },
+
+    resumeRunningTimer() {
+      this.loading = true;
+
+      Nova.request()
+        .get("/nova-vendor/tracked-timesheets/latest-timer/resume")
+        .then(response => {
+          this.runningTimer = response.data;
+
+          if (this.isValid) {
+            this.timeWorked = response.data.time_worked;
+          } else {
+            // TODO: Move to create new.
+          }
+
+          this.loading = false;
+        });
+    },
+
+    stopRunningTimer() {
+      this.loading = true;
+
+      Nova.request()
+        .get("/nova-vendor/tracked-timesheets/latest-timer/stop")
+        .then(response => {
+          this.runningTimer = response.data;
+
+          if (this.isValid) {
+            this.timeWorked = response.data.time_worked;
+          } else {
+            // TODO: Move to create new.
+          }
+
+          this.loading = false;
+        });
+    },
+
+    startTimer() {
+      this.timer = setInterval(() => this.updateTimer(), 1000);
+    },
+
+    updateTimer() {
+      this.timeWorked++;
+    },
+
+    pauseTimer() {
+      this.pauseRunningTimer();
+
+      clearInterval(this.timer);
+      this.timer = null;
+    },
+
+    resumeTimer() {
+      this.resumeRunningTimer();
+      this.startTimer();
+    },
+
+    stopTimer() {
+      this.stopRunningTimer();
+
+      clearInterval(this.timer);
+      this.timer = null;
+    },
+
+    padTime(time) {
+      return (time < 10 ? "0" : "") + time;
+    }
+  },
+
+  computed: {
+    timeWorked() {
+      return this.timeWorked;
+    },
+
+    hours() {
+      return Math.floor(this.timeWorked / 3600);
+    },
+
+    minutes() {
+      return this.padTime(Math.floor(this.timeWorked / 60) % 60);
+    },
+
+    seconds() {
+      return this.padTime(this.timeWorked % 60);
+    },
+
+    isValid() {
+      return this.runningTimer != "{}";
+    },
+
+    isRunning() {
+      return (
+        this.isValid && !this.isEnded && this.runningTimer.paused_at == null
+      );
+    },
+
+    isPaused() {
+      return (
+        this.isValid && !this.isEnded && this.runningTimer.paused_at != null
+      );
+    },
+
+    isEnded() {
+      return this.isValid && this.runningTimer.ended_at != null;
+    }
+  },
+
+  created() {
+    this.initialize();
+  }
+};
 </script>
--- nova-components/TrackedTimesheets/routes/api.php
+++ nova-components/TrackedTimesheets/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Timesheet;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,51 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/endpoint', function (Request $request) {
-//     //
-// });
+Route::get('/latest-timer', function (Request $request) {
+    $currentEntry = GetCurrentEntry();
+
+    return $currentEntry ?? "{}";
+});
+
+Route::get('/latest-timer/pause', function (Request $request) {
+    $currentEntry = GetCurrentEntry();
+
+    if ($currentEntry->ended_at == null && $currentEntry->paused_at == null) {
+        $currentEntry->paused_at = Carbon::now();
+        $currentEntry->save();
+    }
+
+    return $currentEntry ?? "{}";
+});
+
+Route::get('/latest-timer/resume', function (Request $request) {
+    $currentEntry = GetCurrentEntry();
+
+    if ($currentEntry->ended_at == null && $currentEntry->paused_at != null) {
+        $currentEntry->resumed_at = Carbon::now();
+        $currentEntry->paused_at = null;
+        $currentEntry->save();
+    }
+
+    return $currentEntry ?? "{}";
+});
+
+Route::get('/latest-timer/stop', function (Request $request) {
+    $currentEntry = GetCurrentEntry();
+
+    if ($currentEntry->ended_at == null && $currentEntry->paused_at != null) {
+        $currentEntry->ended_at = $currentEntry->resumed_at ?? Carbon::now();
+        $currentEntry->paused_at = null;
+        $currentEntry->save();
+    }
+
+    return $currentEntry ?? "{}";
+});
+
+function GetCurrentEntry() {
+    return Timesheet::where('type', 'tracked')
+        ->where('user_id', Auth::id())
+        ->whereNull('ended_at')
+        ->orderBy('created_at', 'desc')
+        ->first();
+}
